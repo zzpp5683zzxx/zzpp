5a1c1f367fece3def24a171807af0b8ccf8a58fb
zzpp5683zzxx <57783715+zzpp5683zzxx@users.noreply.github.com>
2019-11-15 15:34:03 +0800

    Update test.java

--- src/main/java/com/offcn/controller/test.java
+++ src/main/java/com/offcn/controller/test.java
@@ -2,6 +2,6 @@ package com.offcn.controller;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("123123132132");
+        System.out.println("123123132132qweqewqwewqeqweqe");
     }
 }
